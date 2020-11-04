88ee207671cbe911fe1f0fbf52a35b5e5942aa7a
Charlesccharris <harriscc@student.swosu.edu>
2020-11-04 08:32:39 -0600

    Still don't know how to get the decimal problem working

--- Homework10_16.java
+++ Homework10_16.java
@@ -1,10 +1,34 @@
+import java.math.*;
+
 public class Homework10_16{
 	public static void main(String[] args){
 		/*(Divisible by 2 or 3)
 		Find the first ten numbers with 50 decimal digits
 		that are divisible by 2 or 3.*/
-		StopWatch timer = new StopWatch();
+//		StopWatch timer = new StopWatch();
 
+		BigDecimal number = new BigDecimal("10000000000000000000000000000000000000000000000000");
+		BigDecimal divisorOne = new BigDecimal("2");
+		BigDecimal divisorTwo = new BigDecimal("3");
+		BigDecimal resultOne;
+		BigDecimal resultTwo;
+
+		int numberCount = 0;
+		while(numberCount < 10){
+			resultOne = number.remainder(divisorOne);
+			resultTwo = number.remainder(divisorTwo);
+			if(resultOne.equals(BigDecimal.ZERO) || resultTwo.equals(BigDecimal.ZERO)){
+				System.out.println(number + " is divisible");
+				numberCount++;
+			}
+			else{
+				System.out.println(number + " isn't divisible");
+				numberCount++;
+			}
+			number = number.add(BigDecimal.ONE);
+		}
+
+/*
 		int foundNumbers = 0;
 		double number = 0.1;
 		int dividerOne = 2;
@@ -14,9 +38,9 @@ public class Homework10_16{
 
 		timer.start();
 		while(foundNumbers < 10){
-			resultOne = number / dividerOne;
+			resultOne = number % dividerOne;
 			System.out.println(resultOne);
-			resultTwo = number / dividerTwo;
+			resultTwo = number % dividerTwo;
 			System.out.println(resultTwo);
 			if(resultOne == 0 && resultTwo != 0){
 				System.out.println("Number " + number + " is divisible by " + dividerOne);
@@ -36,5 +60,6 @@ public class Homework10_16{
 			}
 			number += 0.1;
 		}
+*/
 	}
 }
