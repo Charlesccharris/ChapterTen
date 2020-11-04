612eea863eeff41856c743f632f3e75fb3f9f862
Charlesccharris <harriscc@student.swosu.edu>
2020-11-04 00:33:27 -0600

    Added a framework for the decimal division class, if I can figure out how to get it to work

---
+++ DecimalDivision.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class DecimalDivision{
+
+	private BigDecimal startNumber;
+
+	public DecimalDivision(Scanner input){
+		int currentFound = 0;
+
+
+	}
+/*
+		BigDecimal startNumber = new BigDecimal(start);
+		BigDecimal res1;
+		BigDecmial res2;
+		while(currentFound < totalNumbers){
+			for(
+				res1 = startNumber.divide(num1);
+				res2 = startNumber.divide(num2);
+				if(res1 == 0){
+
+				}
+				else if(res2 ==0){
+
+				}
+		}
+	}
+*/
+}
