3e13cafd72e0e973793c0336757e07e0fb4910eb
Charlesccharris <harriscc@student.swosu.edu>
2020-11-04 11:48:00 -0600

    Deleted the DecimalDivision class

--- CheckNumbers.java
+++ CheckNumbers.java
@@ -7,18 +7,14 @@ public class CheckNumbers{
 
 		int choice = 0;
 
-		System.out.print("If you want to divide a decimal, press 1. If you want to divide a whole number, press 2. If you don't want to do either press 0: ");
+		System.out.print("If you want to divide a whole number, press 1. If you don't want to do anything press 0: ");
 		choice = input.nextInt();
 		if(choice == 1){
-			DecimalDivision decimal = new DecimalDivision(input);
-		}
-		else if(choice == 2){
-			System.out.println("A whole number, good for you.");
+			System.out.println("Whole number.");
 			WholeDivision whole = new WholeDivision(input);
 			timer.start();
 			whole.divide();
 			timer.stop();
-			timer.getElapsedTime();
 		}
 		else if(choice == 0){
 			System.out.println("Have a nice day");
