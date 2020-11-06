554068ce28fc8d417512196d743ab7738b0f64cb
Charlesccharris <harriscc@student.swosu.edu>
2020-11-06 00:07:34 -0600

    Added a class to find prime numbers

--- CheckNumbers.java
+++ CheckNumbers.java
@@ -7,14 +7,14 @@ public class CheckNumbers{
 
 		int choice = 0;
 
-		System.out.print("If you want to divide a whole number, press 1. If you don't want to do anything press 0: ");
+		System.out.print("If you want to divide a whole number, press 1.\nIf you want to find prime numbers, press 2"+
+				 "\nIf you don't want to do anything press 0: ");
 		choice = input.nextInt();
 		if(choice == 1){
-			System.out.println("Whole number.");
-			WholeDivision whole = new WholeDivision(input);
-			timer.start();
-			whole.divide();
-			timer.stop();
+			NumberOne(input, timer);
+		}
+		else if(choice == 2){
+			NumberTwo(input, timer);
 		}
 		else if(choice == 0){
 			System.out.println("Have a nice day");
@@ -23,5 +23,21 @@ public class CheckNumbers{
 			System.out.println("Your choice can't be processed right now, please hang-up, and try again.");
 		}
 		input.close();
+	}
+
+	public static void NumberOne(Scanner input, StopWatch timer){
+		System.out.println("Whole number.");
+		WholeDivision whole = new WholeDivision(input);
+		timer.start();
+		whole.divide();
+		timer.stop();
+	}
+
+	public static void NumberTwo(Scanner input, StopWatch timer){
+		System.out.println("Prime number");
+		PrimeGetter prime = new PrimeGetter(input);
+		timer.start();
+		prime.findPrimes();
+		timer.stop();
 	}
 }
